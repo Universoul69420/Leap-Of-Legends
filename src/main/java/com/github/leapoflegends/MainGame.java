8afd9bb53d468b05d4395f224ebaa3060eee5118
Jens van de Putte <jd.vandeputte@student.han.nl>
2025-03-28 09:46:48 +0100

    Verwijder Oude bestanden

--- src/main/java/com/github/leapoflegends/MainGame.java
+++ src/main/java/com/github/leapoflegends/MainGame.java
@@ -6,8 +6,6 @@ import com.github.hanyaeger.api.Size;
 public class MainGame extends YaegerGame {
 
 
-    public static final int SCENE_SELECTION = 0;
-    public static final int SCENE_GRASS_WORLD = 1;
 
     public static void main(String[] args) {
         launch(args);
