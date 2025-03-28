c8f5250a03c945b241e8558c889386691e380802
Jens van de Putte <jd.vandeputte@student.han.nl>
2025-03-28 10:14:40 +0100

    Tilemap voor de grond toegevoegd

---
+++ src/main/java/com/github/leapoflegends/tilemaps/entities/GroundEntity.java
@@ -0,0 +1,14 @@
+package com.github.leapoflegends.tilemaps.entities;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.impl.SpriteEntity;
+
+public class GroundEntity extends SpriteEntity implements Collider {
+
+
+    public GroundEntity(final Coordinate2D location, final Size size, final Size tileSize) {
+        super("entities/World-Tiles1.png", location, size);
+    }
+}
